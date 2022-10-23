79e436efb4afcb64d836d687721d1db3b850a064
Drakaris8866 <artem03romanovskiy@gmail.com>
2022-10-23 17:47:17 +0300

    Оптимизация бандла

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./scss/app.scss";
-import React, {lazy, Suspense} from "react";
+import React, {Suspense} from "react";
 import {Route, Routes} from "react-router-dom";
 import {Header} from "./components/Header";
 import Preloader from "./components/Preloader";
+import Home from "./components/Pages/Home";
+import Cart from "./components/Pages/Cart";
+import PizzaInfo from "./components/PizzaInfo";
 
-const Home = lazy(() => import(/* webpackChunkName: "Home" */ "./components/Pages/Home"))
-const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ "./components/Pages/Cart"))
-const PizzaInfo = lazy(() => import(/* webpackChunkName: "PizzaInfo" */ "./components/PizzaInfo"))
+// const Home = lazy(() => import(/* webpackChunkName: "Home" */ "./components/Pages/Home"))
+// const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ "./components/Pages/Cart"))
+// const PizzaInfo = lazy(() => import(/* webpackChunkName: "PizzaInfo" */ "./components/PizzaInfo"))
 
 
 function App() {
