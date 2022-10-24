00c57ebf2a0c9f6f816a1dbc018265b9a2bc2f30
Lenovo <kazuk2016@mail.ru>
2022-10-24 17:15:06 +0300

    Оптимизация бандла

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import React, {lazy, Suspense} from "react";
 import {Route, Routes} from "react-router-dom";
 import {Header} from "./components/Header";
 import Preloader from "./components/Preloader";
-import Home from "./components/Pages/Home";
-import Cart from "./components/Pages/Cart";
-import PizzaInfo from "./components/PizzaInfo";
+
+
+const Home = lazy(() => import("./components/Pages/Home"))
+const Cart = lazy(() => import("./components/Pages/Cart"))
+const PizzaInfo = lazy(() => import("./components/PizzaInfo"))
 
 
 function App() {
